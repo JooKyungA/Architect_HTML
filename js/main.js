50268a9ba137d2a28e6312c864185eb34ecfcdca
JooKyungA <jooka95@naver.com>
2022-12-06 02:14:33 +0900

    figure 완료(간격 맞춤 제외), about,gallery,awards HTML 간략하게 작성

---
+++ js/main.js
@@ -0,0 +1,71 @@
+
+const slider = document.querySelector("#slider");
+const ul = slider.querySelector("ul");
+const lis = ul.querySelectorAll("li");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+let len = lis.length;
+
+// span
+const pg = document.querySelector(".pagination");
+const sp = pg.querySelector("span");
+const total = lis.length;
+sp.innerText = total;
+
+// strong --> css까지 완성하고 나서 다시 작성해보기
+// const str = pg.querySelector("strong");
+// str.innerText = 
+
+// slider
+let enableClick = true;
+let speed = 400;
+
+init();
+
+next.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (enableClick) {
+    nextslide();
+    enableClink = false;
+  }
+})
+prev.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (enableClick) {
+    prevslide();
+    enableClink = false;
+  }
+})
+
+function init() {
+  ul.style.left = "-100%";
+  // ul.prepend(ul.lastElementChild);
+  ul.style.width = `${100 * len} %`;
+  lis.forEach((el) => {
+    el.style.width = `${100 / len}%`;
+  })
+}
+function nextslide() {
+  new Anim(ul, {
+    prop: 'left',
+    value: "-200%",
+    duration: speed,
+    callback: () => {
+      ul.append(ul.firstElementChild);
+      ul.style.left = "-100%";
+      enableClick = true;
+    }
+  })
+}
+function prevslide() {
+  new Anim(ul, {
+    prop: 'left',
+    value: "0%",
+    duration: speed,
+    callback: () => {
+      ul.style.left = "-100%"
+      ul.prepend(ul.lastElementChild);
+      enableClick = true;
+    }
+  })
+}
