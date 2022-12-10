da4c369c4f3c4307610f57c4d18d70d58569beef
JooKyungA <jooka95@naver.com>
2022-12-10 20:46:41 +0900

    aside 작성완료

--- js/main.js
+++ js/main.js
@@ -9,6 +9,96 @@ for (let el of skipNavi) {
     el.classList.remove("on");
   })
 }
+// view content 관련---------------------
+const btnOpen = document.querySelector(".btnOpen");
+const visual = document.querySelector("#visual");
+const aside = document.querySelector("aside");
+const btnClose = aside.querySelector(".btnClose");
+const _top = aside.querySelector(".top");
+const _right = aside.querySelector(".right");
+const _bottom = aside.querySelector(".bottom");
+const _left = aside.querySelector(".left");
+const _inner = aside.querySelector(".inner");
+// const speed = 500;
+
+btnOpen.addEventListener("click", (e) => {
+  e.preventDefault();
+  aside.style.display = "block";
+  visual.classList.add("off");
+  new Anim(_top, {
+    prop: "width",
+    value: "100%",
+    duration: speed,
+    callback: () => {
+
+      new Anim(_right, {
+        prop: "height",
+        value: "100%",
+        duration: speed,
+        callback: () => {
+          new Anim(_bottom, {
+            prop: "width",
+            value: "100%",
+            duration: speed,
+            callback: () => {
+              new Anim(_left, {
+                prop: "height",
+                value: "100%",
+                duration: speed,
+                callback: () => {
+                  new Anim(_inner, {
+                    prop: "opacity",
+                    value: 1,
+                    duration: speed,
+                  })
+                }
+              })
+
+            }
+          })
+        }
+      })
+    }
+  })
+})
+
+btnClose.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  new Anim(_inner, {
+    prop: "opacity",
+    value: 0,
+    duration: speed,
+    callback: () => {
+      new Anim(_top, {
+        prop: "width",
+        value: "0%",
+        duration: speed,
+      });
+      new Anim(_right, {
+        prop: "height",
+        value: "0%",
+        duration: speed,
+      });
+      new Anim(_bottom, {
+        prop: "width",
+        value: "0%",
+        duration: speed,
+      });
+      new Anim(_left, {
+        prop: "height",
+        value: "0%",
+        duration: speed,
+        callback: () => {
+          aside.style.display = "none";
+
+          visual.classList.remove("off");
+        }
+      });
+    }
+  })
+})
+
 
 // btnCall 관련 js-----------------------
 const btnCall = document.querySelector(".btnCall");
